8f73f5ec995827d3ba23c35c4490d2a18e6e6a4d
KendallMM <kendallmarinmunoz@gmail.com>
2022-05-05 18:48:32 -0600

    Actividad #2

---
+++ web-app/src/App.js
@@ -0,0 +1,55 @@
+//import React, { useState } from 'react';
+import React from 'react';
+import './App.css';
+import { fetchHelloWorld } from './services/HelloWorldService';
+/*let timer;
+function App() {
+  const [currentTime, setCurrentTime]=useState(Date.now);
+  const [label, setLabel] = useState("");
+  const clickHandler = async () => {
+    const result = await fetchHelloWorld();
+    setLabel(result);
+  };
+  if (!timer){
+  timer = setInterval(() =>{
+    setCurrentTime(Date.now());
+  }, 1000)
+}
+  return (
+    <div className="App">
+      {new Date(currentTime).toLocaleString()}
+      <button onClick={() => clickHandler()}>Call API</button>
+      {label}
+    </div>
+  );
+}
+export default App;*/
+export class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      currentTime:Date.now()
+    }
+    setInterval(()=>{
+      this.setState({
+        currentTime:Date.now()
+      });
+    }, 1000)
+  }
+  async clickHandler() {
+    const result = await fetchHelloWorld();
+    this.setState({
+      label:result
+    });
+  }
+  render(){
+    return(
+      <div className='App'>
+        {new Date(this.state.currentTime).toLocaleString()}
+        <button onClick={()=> this.clickHandler()}>Call API</button>
+        {this.state.label}
+      </div>
+    );
+  }
+}
+export default App;
